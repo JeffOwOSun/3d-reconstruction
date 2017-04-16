792de86040a758fda16000f9aa7caef1fcb669ea
JeffOwOSun <sunyushi9546@gmail.com>
2017-04-16 21:39:28 +0800

    initial commit

---
+++ sift.py
@@ -0,0 +1,13 @@
+import cv2
+#import numpy as np
+
+#read an image
+img = cv2.imread('home.jpg')
+#print(img)
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+sift = cv2.xfeatures2d.SIFT_create()
+kp, des = sift.detectAndCompute(gray,None)
+
+img=cv2.drawKeypoints(gray,kp,img,flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+cv2.imwrite('sift_keypoints.jpg',img)
